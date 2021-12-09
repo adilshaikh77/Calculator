79830a2487c5ccd6abfabdd0a0e15f323f0fafd5
adilshaikh77 <adilshaikh77@users.noreply.github.com>
2021-12-09 15:47:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Hello,
+        This was build by Adil Shaihk.
+        You need to run npm scripts to start the program
+        23-05-2021
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Layout from './components/layout';
+
+function App() {
+  return ( 
+    <div classname="App">
+      <Layout></Layout>
+    </div>
+  );
+}
+
+  
+
+export default App;
+
